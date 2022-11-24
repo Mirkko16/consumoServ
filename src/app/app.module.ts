2c577ea1be966b94fa768e0dfb92fcb5ed8b7283
Mirkko16 <mirkko1996@hotmail.com>
2022-11-24 20:36:13 -0300

    primer avance consumo api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/formulario/formulario.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit{
+
+  categoriaSeleccionada = 'general';
+  paisSeleccionado='ar'
+
+  categorias: any[]=[
+    {value:'general',nombre:'General'},
+    {value:'business',nombre:'Negocios'},
+    {value:'entertainment',nombre:'Entretenimientos'},
+    {value:'health',nombre:'Salud'},
+    {value:'science',nombre:'Ciencia'},
+    {value:'sports',nombre:'Deportes'},
+    {value:'technology',nombre:'Tecnologia'}    
+  ]
+
+
+  paises: any[]=[
+    {value:'ar',nombre:'Argentina'},
+    {value:'br',nombre:'Brasil'},
+    {value:'fr',nombre:'Francia'},
+    {value:'hu',nombre:'Hungria'},
+    {value:'mx',nombre:'Mexico'},
+    {value:'gb',nombre:'Reino Unido'}
+  ]
+  
+  constructor() { }    
+
+  ngOnInit(): void {
+    
+  }
+
+}
